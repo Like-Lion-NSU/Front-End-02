ef3807c3defa9bb95307051640ef8c1249a8f0fd
정재은 <jjyjjy0528@naver.com>
2023-05-16 02:18:18 +0900

    네이버 끝

--- 정재은/네이버 클론 코딩/naver.js
+++ 정재은/네이버 클론 코딩/naver.js
@@ -5,6 +5,9 @@ const $inputName = document.querySelector("#name")
 const $inputYear = document.querySelector("#year")
 const $inputMonth = document.querySelector(".month")
 const $inputDay = document.querySelector(".day")
+const $inputEmail = document.querySelector("#email")
+const $buttonPhone=document.querySelector(".numbutton")
+const $inputPhone=document.querySelector(".phonenum")
 
 const rock1 = document.querySelector(".rock")
 const rock2 = document.querySelector(".rock2")
@@ -14,12 +17,16 @@ const text1 = document.createElement('p');
 const text2 = document.createElement('p');
 const text3 = document.createElement('p');
 const text4 = document.createElement('p');
+const text5 = document.createElement('p');
+const text6=document.createElement('p');
 
 const block_0 = document.querySelector(".id1")
 const block_1 = document.querySelector("#PWD")
 const block_2 = document.querySelector("#PWD2")
 const block_3 = document.querySelector(".name1")
 const block_4 = document.querySelector(".bDay")
+const block_5=document.querySelector("#MAIL")
+const block_6=document.querySelector(".phone0")
 
 function ID_CHECK(event){
     var regID = /^[a-z0-9_-]{5,20}$/;
@@ -45,11 +52,12 @@ function PWD_CHECK(event) {
 }
 function PWD_RECHECK(event) {
     const recheck = event.target.value;
-    const check = $input1.value;
+    const check = $inputPass.value;
     if (recheck !== check) {
         text2.textContent = '비밀번호가 일치하지 않습니다.'
         text2.style.color = 'red'
         block_2.appendChild(text2)
+        rock2.style.backgroundPosition= "25% 0"
     }
     else{
         rock2.style.backgroundPosition = "79% 0%"
@@ -108,6 +116,28 @@ function DAY_CHECK(event){
         text4.remove()
     }
 }
+function EMAIL_CHECK(event){
+    var regEmail=/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+    if(regEmail.test(event.target.value)===false){
+        text5.textContent = '이메일 주소를 다시 확인해주세요.'
+        text5.style.color = 'red'
+        block_5.appendChild(text5)
+    } else {
+        text5.remove()
+    }
+}
+function PHONE_CHECK(event){
+    const phoneValue = $inputPhone.value;
+    var regPhone=/^(01[016789])(\d{3,4})(\d{4})$/;
+    if(regPhone.test(phoneValue)===false){
+        text6.textContent = '형식에 맞지 않는 번호입니다.'
+        text6.style.color = 'red'
+        block_6.appendChild(text6)
+    } else {
+        text6.remove()
+    }
+
+}
 $inputId.addEventListener("change", ID_CHECK)
 $inputPass.addEventListener("change", PWD_CHECK) //커서가 다른 곳으로 떠났을 때
 $inputPass2.addEventListener("change", PWD_RECHECK)
@@ -115,3 +145,5 @@ $inputName.addEventListener("change", NAME_CHECK)
 $inputYear.addEventListener("change",YEAR_CHECK)
 $inputMonth.addEventListener("change", MONTH_CHECK);
 $inputDay.addEventListener("change", DAY_CHECK);
+$inputEmail.addEventListener("change",EMAIL_CHECK);
+$buttonPhone.addEventListener("click",PHONE_CHECK);
